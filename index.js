106e0d2f341b8d55aa1f28bb475f41e07c83e009
miloscomplex <hicallmesutton@gmail.com>
2021-01-15 01:05:22 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function isWin(obj) {
+  return obj.result === 'W'
+}
+
+function superbowlWin(array) {
+  let result = array.find(isWin)
+  if (!!result) {
+    return result.year
+  } else {
+    result
+  }
+}
